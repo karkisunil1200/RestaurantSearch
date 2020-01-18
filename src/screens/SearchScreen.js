487fc55dd28e49b4be087c0a111a0cf3fe4ac4e3
Sunil Karki <karkisunil1200@gmail.com>
2020-01-18 10:02:55 -0600

    Getting started

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Hello from search screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default SearchScreen;
